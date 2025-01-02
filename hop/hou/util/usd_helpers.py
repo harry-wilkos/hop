55019351e1888b90f3dcd3921a42b05c99eff172
harry-wilkos <hg456wilkos@gmail.com>
2025-01-02 20:45:19 +0000

    cleaning usd stage function

--- hop/hou/util/usd_helpers.py
+++ hop/hou/util/usd_helpers.py
@@ -1,9 +1,25 @@
 from pxr.Usd import Stage, Prim
+from pxr import UsdGeom
 
 
-def expand_stage(stage: Stage) -> Prim:
-    prims_stack = [stage.GetPrimAtPath("/")]
-    while prims_stack:
-        current_prim = prims_stack.pop()
+def expand_stage(stage: Stage, depth: int | None = None) -> Prim:
+    queue = [(stage.GetPrimAtPath("/"), 0)]
+    while queue:
+        current_prim, current_depth = queue.pop(0)
         yield current_prim
-        prims_stack.extend(current_prim.GetChildren())
+        if depth is not None and current_depth >= depth:
+            continue
+        queue.extend((child, current_depth + 1) for child in current_prim.GetChildren())
+
+
+def clean_stage(stage: Stage) -> None:
+    all_prims = list(expand_stage(stage))
+    all_prims.reverse()
+    for prim in all_prims:
+        if prim.IsPseudoRoot():
+            continue
+        if prim.GetTypeName() in {"Xform", "Scope", ""}:
+            if not prim.GetChildren():
+                prim.GetStage().RemovePrim(prim.GetPath())
+            elif all(child.IsInstanceProxy() for child in prim.GetChildren()):
+                prim.GetStage().RemovePrim(prim.GetPath())
