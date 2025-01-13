f8ede76b8e139fd848f577f40725ef7863bf4828
harry-wilkos <hg456wilkos@gmail.com>
2025-01-13 16:23:21 +0000

    stage updates

--- hop/hou/hdas/stage.py
+++ hop/hou/hdas/stage.py
@@ -1,8 +1,11 @@
 from hop.hou.util.usd_helpers import compare_scene
 import os
+import hou
 
 def export(kwargs: dict) -> None:
     node = kwargs["node"]
+    node.parm("current_frame").set(hou.frame())
+    node.parm("rendering").set(1)
 
     tags = node.node("LPE_Tag")
     tags.parm("manualtags").set(0)
@@ -13,27 +16,28 @@ def export(kwargs: dict) -> None:
     resources_path = resources_node.evalParm("savepath")
     resources_stage = resources_node.stage()
     if os.path.exists(resources_path) and compare_scene(resources_stage, resources_path):
-        node.node("Shot_Resources").parm("reload").pressButton()
+        node.parm("Reload_Resources").pressButton()
     else:
-        node.node("Export_Resources").parm("execute").pressButton()
+        node.parm("Export_Resources").pressButton()
     
     settings_node = node.node("Shot_Settings_Save")
     settings_path = settings_node.evalParm("savepath")
     settings_stage = settings_node.stage()
     if os.path.exists(settings_path) and compare_scene(settings_stage, settings_path):
-        node.node("Shot_Settings").parm("reload").pressButton()
+        node.parm("Reload_Settings").pressButton()
     else:
-        node.node("Export_Settings").parm("execute").pressButton()
+        node.parm("Export_Settings").pressButton()
 
     assets_node = node.node("Shot_Assets_Save")
     assets_path = assets_node.evalParm("savepath")
     assets_stage = assets_node.stage()
     if os.path.exists(assets_path) and compare_scene(assets_stage, assets_path):
-        node.node("Shot_Assets").parm("reload").pressButton()
+        node.parm("Reload_Assets").pressButton()
     else:
-        node.node("Export_Assets").parm("execute").pressButton()
+        node.parm("Export_Assets").pressButton()
 
     node.node("Export_USD").parm("execute").pressButton()
+    node.parm("rendering").set(0)
 
 
 def clear_aov(kwargs: dict) -> None:
