c96b6346c67b4682d3df4f36c22535fb5fc316f8
harry-wilkos <hg456wilkos@gmail.com>
2025-01-20 13:12:20 +0000

    env fix v3

--- hop/dl/util/helpers.py
+++ hop/dl/util/helpers.py
@@ -3,4 +3,7 @@ import os
 
 def set_env(vars: list):
     for index, var in enumerate(vars):
-        yield f"EnvironmentKeyValue{index}={var}={os.environ[var]}"
+        value = os.environ[var]
+        if os.path.exists(value):
+            value = os.path.normpath(value)
+        yield f"EnvironmentKeyValue{index}={var}={value}"
