db8749803c3c6e5d607b08c5530f8642cb613e6f
harry-wilkos <hg456wilkos@gmail.com>
2025-02-11 19:33:57 +0000

    render rop

--- hop/hou/hdas/__init__.py
+++ hop/hou/hdas/__init__.py
@@ -1,3 +1,3 @@
-from hop.hou.hdas import shot, stage, disk_cache
+from hop.hou.hdas import shot, stage, disk_cache, karma_rop
 
-__all__ = ["shot", "stage", "disk_cache"]
+__all__ = ["shot", "stage", "disk_cache", "karma_rop"]
---
+++ hop/hou/hdas/karma_rop.py
@@ -0,0 +1,33 @@
+def find_aovs(kwargs: dict):
+    node = kwargs["node"]
+    node.parm("rendervars").set(0)
+    for folder in [
+        "Colour",
+        "Diffuse",
+        "Reflections & Refractions",
+        "Lights & Emission",
+        "Volume",
+        "BSDF Labels",
+        "Ray",
+        "Crypto",
+    ]:
+        parms = node.parmsInFolder(("Settings", "AOVs", folder))
+        for parm in parms:
+            yield parm
+
+
+def clear_aov(kwargs: dict) -> None:
+    for parm in find_aovs(kwargs):
+        parm.set(0)
+    kwargs["node"].parm("beauty").set(1)
+
+def export(kwargs: dict):
+    node = kwargs["node"]
+    tags = node.node("LPE_Tag")
+    tags.parm("manualtags").set(0)
+    if node.evalParm("preprocess") != 0:
+        tags.parm("populate").pressButton()
+
+def local_render(kwargs:dict):
+    export(kwargs) 
+
