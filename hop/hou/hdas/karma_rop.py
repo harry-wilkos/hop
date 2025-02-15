2272f5d070a91d729c22cbee254ae10c60625794
harry-wilkos <hg456wilkos@gmail.com>
2025-02-15 14:21:42 +0000

    karma rop

--- hop/hou/hdas/karma_rop.py
+++ hop/hou/hdas/karma_rop.py
@@ -1,5 +1,7 @@
 import os
 from hop.hou.util.usd_helpers import compare_scene
+from time import sleep
+
 
 def find_aovs(kwargs: dict):
     node = kwargs["node"]
@@ -24,6 +26,7 @@ def clear_aov(kwargs: dict) -> None:
         parm.set(0)
     kwargs["node"].parm("beauty").set(1)
 
+
 def export(kwargs: dict):
     node = kwargs["node"]
     tags = node.node("LPE_Tag")
@@ -31,17 +34,37 @@ def export(kwargs: dict):
     if node.evalParm("preprocess") != 0:
         tags.parm("populate").pressButton()
 
-    settings_export = node.node("set_Path2")
+    settings_export = node.node("Set_Path2")
     settings_path = settings_export.evalParm("savepath")
     settings_stage = settings_export.stage()
-    if os.path.exists(settings_path) and compare_scene(settings_stage, settings_path):
-        node.parm("")
+    if not (os.path.exists(settings_path)) or not (
+        compare_scene(settings_stage, settings_path)
+    ):
+        node.parm("export_settings").pressButton()
+
+    scene_export = node.node("Set_Path1")
+    scene_path = scene_export.evalParm("savepath")
+    scene_stage = scene_export.stage()
+    if not (os.path.exists(scene_path)) or not (compare_scene(scene_stage, scene_path)):
+        node.parm("export_scene").pressButton()
+
+    node.parm("reload").pressButton()
+    node.parm("export_render").pressButton()
 
 
-    render_export = node.node("Import_Layers")
-    settings_path = render_export.evalParm("filepath1")
+def local_render(kwargs: dict):
+    node = kwargs["node"]
+    node.parm("targettopnetwork").set("Local_Render")
+    export(kwargs)
+    node.parm("dirty").pressButton()
+    node.parm("cook").pressButton()
 
 
-def local_render(kwargs:dict):
-    export(kwargs) 
-
+def clear_cache(kwargs: dict):
+    node = kwargs["node"]
+    node.parm("cancel").pressButton()
+    node.parm("targettopnetwork").set("")
+    for i in range(1, 4):
+        path = node.node(f"Set_Path{i}").evalParm("savepath")
+        if os.path.exists(path):
+            os.remove(path)
